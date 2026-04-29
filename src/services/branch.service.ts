6b2a02eeaf9430fea60fbf9eae80ed429b381c84
huysun <huysun2511@gmail.com>
2026-04-29 20:38:55 +0700

    feat(branch): add sub components

--- src/services/branch.service.ts
+++ src/services/branch.service.ts
@@ -2,21 +2,23 @@ import { ApiResponse } from "@/constants/apiResponse";
 import { Branch, BranchInput, BranchParams } from "@/schemas/branch.schema";
 import http from "@/utils/http";
 
+const prefix = "/branches";
+
 const branchService = {
   getBranches: (params?: BranchParams) => {
-    return http.get<ApiResponse<Branch[]>>("/branches", { params });
+    return http.get<ApiResponse<Branch[]>>(prefix, { params });
   },
 
   createBranch: (data: BranchInput) => {
-    return http.post<ApiResponse<Branch>>("/branches", data);
+    return http.post<ApiResponse<Branch>>(prefix, data);
   },
 
   updateBranch: (id: string, data: Partial<BranchInput>) => {
-    return http.patch<ApiResponse<Branch>>(`/branches/${id}`, data);
+    return http.patch<ApiResponse<Branch>>(`${prefix}/${id}`, data);
   },
 
   deleteBranch: (id: string) => {
-    return http.delete<ApiResponse<null>>(`/branches/${id}`);
+    return http.delete<ApiResponse<null>>(`${prefix}/${id}`);
   },
 };
 
